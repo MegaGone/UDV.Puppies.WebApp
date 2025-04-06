9f903025bd1ab5954b8ecebf48e40557ba064248
MegaGone <jimmymartinez016@gmail.com>
2025-04-05 22:25:34 -0600

    [FEAT] - Setup pagination

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,15 +6,19 @@ import { CommonModule } from '@angular/common';
 import { IPuppy } from '../../interfaces/puppy.interface';
 import { CardComponent } from '../../components/card/card.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'home',
   standalone: true,
   styleUrl: './home.component.sass',
   templateUrl: './home.component.html',
-  imports: [CommonModule, HeaderComponent, CardComponent],
+  imports: [CommonModule, HeaderComponent, CardComponent, MatButtonModule],
 })
 export class HomeComponent implements OnInit {
+  private _currentPage = 0;
+  private readonly _pageSize = 12;
+
   private readonly _puppyService = inject(HomeService);
   public puppies: Array<Partial<IPuppy>>;
 
@@ -23,12 +27,24 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this._getPuppiesPaginated(0, 12);
+    this._getPuppiesPaginated(this._currentPage, this._pageSize);
   }
 
   private _getPuppiesPaginated(page: number = 0, pageSize: number = 10): void {
     this._puppyService.findPaginated(page, pageSize).subscribe((res) => {
       this.puppies = res;
     });
+  }
+
+  public onNextPage(): void {
+    this._currentPage += 1;
+    this._getPuppiesPaginated(this._currentPage, this._pageSize);
+  }
+
+  public onPreviousPage(): void {
+    if (this._currentPage <= 0) return;
+
+    this._currentPage -= 1;
+    this._getPuppiesPaginated(this._currentPage, this._pageSize);
   }
 }
